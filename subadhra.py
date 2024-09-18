7550f49506dde60c76bda75ed4111b9042f1c0bf
abhijeet198525 <165279787+abhijeet198525@users.noreply.github.com>
2024-09-18 09:34:47 +0530

    Update subadhra.py

--- subadhra.py
+++ subadhra.py
@@ -1,1 +1,20 @@
 we should learn basic git commands, its a mandatoty for Da employee
+git commands
+========================
+git clone
+git fetch
+git pull
+git push
+git commit
+git log
+git reflog
+git init
+git stash
+git revert
+git reset 
+git diff
+git branch
+git checkout
+git cherry-pick
+git tag
+
