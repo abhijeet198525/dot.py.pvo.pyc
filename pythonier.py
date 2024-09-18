7e8aa12a6882d018cd05df6f53a5bc8694bb147b
abhijeet198525 <165279787+abhijeet198525@users.noreply.github.com>
2024-09-18 09:36:41 +0530

    Update pythonier.py

--- pythonier.py
+++ pythonier.py
@@ -1,2 +1,6 @@
-git commands
-========================
+*using cat create multiple file
+cat one.py two.py
+
+*tee is command we can write a content in multiple file at a time
+tee one.py two.py   
+print("hello python")
