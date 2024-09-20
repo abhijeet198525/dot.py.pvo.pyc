7444c20b4e0d71476626572b0012544d70ffcac1
abhijeet198525 <165279787+abhijeet198525@users.noreply.github.com>
2024-09-20 08:17:50 +0530

    Update subadhra.py

--- subadhra.py
+++ subadhra.py
@@ -1,11 +1,17 @@
 we should learn basic git commands, its a mandatoty for Da employee
 git commands
 ========================
-git clone
-git fetch
-git pull
+git clone  # it will download the entire repository as a folder 
+git add
+git status
+git fetch  #it will give the latest information what happend, it will not download it will give rumors and gossip
+           #like a facebook, there is a one thred ""poke"" its 
+git pull  # it will download the difference changes in the repository.
 git push
 git commit
+git config --global user.name 
+git config --global user.email
+git config --list
 git log
 git reflog
 git init
@@ -17,4 +23,5 @@ git branch
 git checkout
 git cherry-pick
 git tag
+
 
